245d3ea5420039646a465ce66d8193400987a771
Milan Kostadinovic <kostadinovicnis@gmail.com>
2022-02-14 19:14:33 +0100

    LocationPopupUpPage

--- src/pages/LocationPopupPage.java
+++ src/pages/LocationPopupPage.java
@@ -1,5 +1,17 @@
 package pages;
 
-public class LocationPopupPage {
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class LocationPopupPage extends BasicPage {
+
+	public LocationPopupPage(WebDriver driver, JavascriptExecutor js, WebDriverWait waiter) {
+		super(driver, js, waiter);
+		
+	}
+	
+	
+	
+	
 }
